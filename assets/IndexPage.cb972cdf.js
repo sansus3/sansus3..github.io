e9a3d6016b4d88bbe0feb03a5a4e9f622959a312
webferrol <xurxo@webferrol.com>
2022-09-16 17:10:31 +0200

    deploy perfil de usuario

---
+++ assets/IndexPage.cb972cdf.js
@@ -0,0 +1,1 @@
+import{Q as e}from"./QPage.a7e31bdc.js";import{_ as a,E as t,F as s,G as r,H as o}from"./index.64cc4690.js";const c={},n=o("h1",null,"Home",-1);function _(d,f){return t(),s(e,{padding:"",class:"row items-center justify-evenly"},{default:r(()=>[n]),_:1})}var m=a(c,[["render",_]]);export{m as default};
---
+++ assets/notify.hook.f33a760a.js
@@ -0,0 +1,1 @@
+import{i as e,ay as t}from"./index.64cc4690.js";function n(){return e(t)}const c=()=>{const r=n();return{error:o=>r.notify({color:"red-5",textColor:"white",icon:"warning",message:`${o}`}),ok:o=>r.notify({color:"green-5",textColor:"white",icon:"done",message:`${o}`})}};export{c as u};
